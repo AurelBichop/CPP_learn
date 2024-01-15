03d43017a87e2e06338c770c3534cdb4e153fe3f
AurelBichop <linux.auelien@gmail.com>
2024-01-15 22:14:44 +0100

    refactorisation premier niveau

--- Le juste Prix/main.cpp
+++ Le juste Prix/main.cpp
@@ -2,49 +2,70 @@
 
 using namespace std;
 
+enum class ChoixMenu{
+  JOUER = 'j',
+  QUITTER = 'q',
+  INCORRECT
+};
+
 int main()
 {
+    const auto BORNE_MIN{0};
+    const auto BORNE_MAX{10'000};
+
+
     cout << "Bienvenue au juste prix" << endl;
-    cout << "j: pour jouer" << endl;
-    cout << "q: pour quitter" << endl;
-    auto choix{'j'};
-    cin >> choix;
+    cout << static_cast<char>(ChoixMenu::JOUER) << " : pour jouer" << endl;
+    cout << static_cast<char>(ChoixMenu::QUITTER) << " : pour quitter" << endl;
+    auto choix{ChoixMenu::JOUER};
+    char saisieChoix;
+    cin >> saisieChoix;
+
+    if(saisieChoix == static_cast<char>(ChoixMenu::JOUER) || saisieChoix == static_cast<char>(ChoixMenu::QUITTER))
+    {
+        choix = static_cast<ChoixMenu>(saisieChoix);
+    }else{
+        choix = ChoixMenu::INCORRECT;
+    }
 
 
     switch(choix){
-    case 'j':
-        cout << "C'est parti !" << endl;
-        for(auto aDeviner: {2018, 42, 1984}){
-            auto proposition{0};
-            auto nombreTentatives{0};
+        case ChoixMenu::JOUER:
+            cout << "C'est parti !" << endl;
+            for(auto aDeviner: {2018, 42, 1984}){
+                auto proposition{0};
+                auto nombreTentatives{0};
 
-            do{
-                cin >> proposition;
+                do{
+                    cin >> proposition;
 
-                if(proposition >= 0 && proposition < 10'000){
-                    nombreTentatives++;
-                    if(proposition == aDeviner){
-                        cout << "Bravo" << endl;
-                    }else if(proposition > aDeviner){
-                        cout << "C'est moins" << endl;
-                    }else{
-                        cout << "C'est plus" << endl;
+                    if(proposition >= BORNE_MIN && proposition < BORNE_MAX){
+                        nombreTentatives++;
+                        if(proposition == aDeviner){
+                            cout << "Bravo" << endl;
+                        }else if(proposition > aDeviner){
+                            cout << "C'est moins" << endl;
+                        }else{
+                            cout << "C'est plus" << endl;
+                        }
                     }
-                }
-            } while(proposition != aDeviner && proposition >= 0);
+                } while(proposition != aDeviner && proposition >= 0);
 
-            if(proposition == aDeviner){
-                cout << "Partie terminée ! " << "en " << nombreTentatives << " tentatives" << endl;
-            }else{
-                cout << "Partie abandonnée ..." << endl;
+                if(proposition == aDeviner){
+                    cout << "Partie terminée ! " << "en " << nombreTentatives << " tentatives" << endl;
+                }else{
+                    cout << "Partie abandonnée ..." << endl;
+                }
             }
-        }
-        break;
-    case 'q':
-        cout << "Au revoir !" << endl;
-        break;
-    default:
-        cout << "Au revoir !" << endl;
-        break;
+            break;
+        case ChoixMenu::QUITTER:
+            cout << "Au revoir !" << endl;
+            break;
+        case ChoixMenu::INCORRECT:
+            cout << "Incorrect !" << endl;
+            break;
+        default:
+            cout << "Au revoir !" << endl;
+            break;
     }
 }
